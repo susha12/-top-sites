4404a4662f150e318647975eae57a84c2476a147
susha12 <87581584+susha12@users.noreply.github.com>
2022-03-13 08:54:31 +0400

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,126 @@
+let baseHumor = [
+    number1 = { 
+    name: "fishki.net", 
+    link: "https://fishki.net/",
+    img:"/img/imajes_humor/1.png",
+    text: "Приколы, юмор, шутки, смешные фото и видео для хорошего настроения на развлекательном портале Fishki.net." 
+  },
+    number2 = { 
+    name: "yaplakal.com", 
+    link: "https://yaplakal.com/",
+    img:"/img/imajes_humor/2.png",
+    text: "ЯПлакалъ - развлекательное сообщество"  
+  },
+    number3 = { 
+    name: "jpozitivim.com", 
+    link: "https://pozitivim.com/",
+    img:"/img/imajes_humor/3.png",
+    text: "Делимся добрыми новостями, рассказываем об интересном, поднимаем настроение. Позитивим!" 
+  },
+    number4 = { 
+    name: "anekdot.ru", 
+    link: "https://www.anekdot.ru/",
+    img:"/img/imajes_humor/4.png",
+    text: "Анекдоты из России - самые смешные анекдоты, истории, фразы и афоризмы, стишки, карикатуры и другой юмор." 
+  },
+    number5 = { 
+    name: "stuki-druki.com", 
+    link: "https://stuki-druki.com/",
+    img:"/img/imajes_humor/5.png",
+    text: "смешных, милых, прикольных картинок, фотографий, фотожаб, видео" 
+  },
+    number6 = { 
+    name: "bipbap.ru", 
+    link: "https://bipbap.ru/",
+    img:"/img/imajes_humor/6.png",
+    text: "Прикольные картинки и качественный юмор бесплатно. Ежедневно поступают новые изображения для срисовки, поздравлений и других красивых фоток."  
+  },
+    number7 = { 
+    name: "ucrazy.ru", 
+    link: "https://ucrazy.ru/",
+    img:"/img/imajes_humor/7.png",
+    text: "Юмор и развлечения. Фото, видео приколы. Анекдоты, рассказы, шутки, веселые истории. Флеш игры и мультики" 
+  },
+    number8 = { 
+    name: "pressa.tv", 
+    link: "https://pressa.tv/",
+    img:"/img/imajes_humor/8.png",
+    text: "Истории из жизни, мемы, свежие анекдоты, красивые девушки и смешные видео поднимающие настроение."  
+  },
+    number9 = { 
+    name: "bash.org.ru", 
+    link: "https://bash.org.ru/",
+    img:"/img/imajes_humor/9.png",
+    text: "развлекательный портал рунета!" 
+  },
+    number10 = { 
+    name: "zagony.ru", 
+    link: "https://zagony.ru/",
+    img:"/img/imajes_humor/10.png",
+    text: "Ежедневное добавление прикольных картинок, смешных демотиваторов и видео приколов." 
+  },
+    number11 = { 
+    name: "bugaga.ru", 
+    link: "https://bugaga.ru/",
+    img:"/img/imajes_humor/11.png",
+    text: "На БУГАГА.ру всегда смешные видео и фото-приколы, также другие разнообразные прикольные картинки и свежие анекдоты." 
+  },
+    number12 = { 
+    name: "ekabu.ru", 
+    link: "https://ekabu.ru/",
+    img:"/img/imajes_humor/12.png",
+    text: "Подборка подарков 2021 от Екабу ❘ фото ... С добрым утром, Екабу" 
+  },
+    number13 = { 
+    name: "klike.net", 
+    link: "https://klike.net/",
+    img:"/img/imajes_humor/13.png",
+    text: "На сайте Клайк вы найдете прикольные картинки, красивые открытки, рисунки для срисовки и позитив." 
+  },
+    number14 = { 
+    name: "yaustal.com", 
+    link: "https://yaustal.com/",
+    img:"/img/imajes_humor/14.png",
+    text: "YaUstal.com - развлекательный портал рунета! Ежедневные подборки прикольных фотографий, смешных картинок, тонны юмора и приколов!" 
+  },
+    number15 = { 
+    name: "mirpozitiva.ru", 
+    link: "https://www.mirpozitiva.ru/",
+    img:"/img/imajes_humor/15.png",
+    text: "Сайт позитива и хорошего настроения."  
+  },
+    number16 = { 
+    name: "zadolba.li", 
+    link: "https://zadolba.li/",
+    img:"/img/imajes_humor/16.png",
+    text: "Тематические подборки фото-приколов" 
+  },
+    number17 = { 
+    name: "anekdotovstreet.com", 
+    link: "https://anekdotovstreet.com/",
+    img:"/img/imajes_humor/17.png",
+    text: "На онлайн сайте «Анекдотов стрит» вы можете совершенно бесплатно читать анекдоты на самые разные темы." 
+  },
+    number18 = { 
+    name: "prykoly.ru", 
+    link: "https://prykoly.ru/",
+    img:"/img/imajes_humor/18.png",
+    text: "Прикольные и смешные картинки с надписями про жизнь" 
+  },
+    number19 = { 
+    name: "vjoy.cc", 
+    link: "https://vjoy.cc/",
+    img:"/img/imajes_humor/19.png",
+    text: "Развлекательные картинки портала VJOY поднимут вам настроение," 
+  },
+  number20 = { 
+    name: "kaifolog.ru", 
+    link: "http://kaifolog.ru/",
+    img:"/img/imajes_humor/20.png",
+    text: "Лучшие приколы. Отборный юмор в больших и качественных подборках." 
+  }
+];
+
+// база цветов
+let colorsOne = ['#FFAA00','#98ED00','#4F10AD','#FF0000	','#9FEE00','#3515B0','#FE7276','#ffffff','#67E46F','#7872D8','#8942D6','#FFF240','#FF4F00','#ffffff','#88BFE2','#7109AA','#1142AA','#E9E8A5','#3AA6D0','#000000'];
+let colorsTwo = ['#FF7673','#D0006E','#FFDE73','#00CC00','#CD0074	','#FFD100','#6EE768','#996AD6','#F0EEE9','#FFF673','#FFF240','#8942D6','#00AC6B','#FFA340','#F80012','#FFFF00','#B9F73E','#FF7373','#9FEE00','#9F69D6'];
